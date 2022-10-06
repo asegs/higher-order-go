b6ec75b80a1d718cfd5791a7d046eeb13eaf06ad
Aaron Segal <ajs2290@g.rit.edu>
2022-10-06 14:30:10 -0400

    Initial commit

---
+++ higher.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"errors"
+)
+
+func Filter[V any](items []V, test func(V) bool) []V {
+	results := make([]V, 0)
+	for _, item := range items {
+		if test(item) {
+			results = append(results, item)
+		}
+	}
+	return results
+}
+
+func Map[V any, T any](items []V, conversion func(V) T) []T {
+	results := make([]T, len(items))
+	for i, item := range items {
+		results[i] = conversion(item)
+	}
+	return results
+}
+
+func Reduce[V any, A any](items []V, operation func(V, A) A, startingAccumulator A) A {
+	for _, item := range items {
+		startingAccumulator = operation(item, startingAccumulator)
+	}
+	return startingAccumulator
+}
+
+func AnyMatch[V any](items []V, test func(V) bool) bool {
+	for _, item := range items {
+		if test(item) {
+			return true
+		}
+	}
+	return false
+}
+
+func AllMatch[V any](items []V, test func(V) bool) bool {
+	for _, item := range items {
+		if !test(item) {
+			return false
+		}
+	}
+	return true
+}
+
+func NoneMatch[V any](items []V, test func(V) bool) bool {
+	for _, item := range items {
+		if test(item) {
+			return false
+		}
+	}
+	return true
+}
+
+func FindFirst[V any](items []V, test func(V) bool) (V, error) {
+	for _, item := range items {
+		if test(item) {
+			return item, nil
+		}
+	}
+	return nil, errors.New("no matches found")
+}
