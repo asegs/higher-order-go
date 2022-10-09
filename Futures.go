c18dfb3e62857b80ad1dae3430a6bf16a2f4d6da
Aaron Segal <ajs2290@g.rit.edu>
2022-10-09 12:14:10 -0400

    Added futures

--- Futures.go
+++ Futures.go
@@ -1,1 +1,50 @@
 package main
+
+import "time"
+
+type Future[T any] struct {
+	channel  chan bool
+	Value    T
+	Finished bool
+}
+
+func Compute[T any](f func() T) *Future[T] {
+	channel := make(chan bool, 1)
+	future := &Future[T]{channel: channel, Finished: false}
+	go func() {
+		v := f()
+		future.channel <- true
+		future.Value = v
+		future.Finished = true
+	}()
+	return future
+}
+
+func (f *Future[T]) Done() bool {
+	return f.Finished
+}
+
+func (f *Future[T]) Get() T {
+	if f.Done() {
+		return f.Value
+	}
+	<-f.channel
+	return f.Value
+}
+
+func (f *Future[T]) GetWithTimeout(duration time.Duration) Optional[T] {
+	if f.Done() {
+		return Of(f.Value)
+	}
+	timeout := make(chan bool, 1)
+	go func() {
+		time.Sleep(duration)
+		timeout <- true
+	}()
+	select {
+	case <-timeout:
+		return None[T]()
+	case <-f.channel:
+		return Of(f.Value)
+	}
+}
