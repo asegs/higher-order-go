98c1bab123bc2038021d03eccefebd7acd0b41f6
Aaron Segal <ajs2290@g.rit.edu>
2022-10-09 13:54:10 -0400

    Added executors

--- Futures.go
+++ Futures.go
@@ -2,29 +2,43 @@ package main
 
 import "time"
 
-type Future[T any] struct {
+type Future[T any] interface {
+	Done() bool
+	Get() T
+	GetTimeout(t time.Duration) Optional[T]
+	Complete(v T)
+}
+
+type AsyncFuture[T any] struct {
 	channel  chan bool
 	Value    T
 	Finished bool
 }
 
-func Compute[T any](f func() T) *Future[T] {
+type CompletableFuture[T any] struct {
+	channel  chan bool
+	Value    T
+	Finished bool
+}
+
+func (f *AsyncFuture[T]) Complete(v T) {
+	f.channel <- true
+	f.Value = v
+	f.Finished = true
+}
+
+func ComputeAsync[T any](f func() T) *AsyncFuture[T] {
 	channel := make(chan bool, 1)
-	future := &Future[T]{channel: channel, Finished: false}
-	go func() {
-		v := f()
-		future.channel <- true
-		future.Value = v
-		future.Finished = true
-	}()
+	future := &AsyncFuture[T]{channel: channel, Finished: false}
+	go future.Complete(f())
 	return future
 }
 
-func (f *Future[T]) Done() bool {
+func (f *AsyncFuture[T]) Done() bool {
 	return f.Finished
 }
 
-func (f *Future[T]) Get() T {
+func (f *AsyncFuture[T]) Get() T {
 	if f.Done() {
 		return f.Value
 	}
@@ -32,7 +46,47 @@ func (f *Future[T]) Get() T {
 	return f.Value
 }
 
-func (f *Future[T]) GetWithTimeout(duration time.Duration) Optional[T] {
+func (f *AsyncFuture[T]) GetWithTimeout(duration time.Duration) Optional[T] {
+	if f.Done() {
+		return Of(f.Value)
+	}
+	timeout := make(chan bool, 1)
+	go func() {
+		time.Sleep(duration)
+		timeout <- true
+	}()
+	select {
+	case <-timeout:
+		return None[T]()
+	case <-f.channel:
+		return Of(f.Value)
+	}
+}
+
+func (f *CompletableFuture[T]) Complete(v T) {
+	f.channel <- true
+	f.Value = v
+	f.Finished = true
+}
+
+func ComputeCompletable[T any](f func() T) *CompletableFuture[T] {
+	channel := make(chan bool, 1)
+	return &CompletableFuture[T]{channel: channel}
+}
+
+func (f *CompletableFuture[T]) Done() bool {
+	return f.Finished
+}
+
+func (f *CompletableFuture[T]) Get() T {
+	if f.Done() {
+		return f.Value
+	}
+	<-f.channel
+	return f.Value
+}
+
+func (f *CompletableFuture[T]) GetWithTimeout(duration time.Duration) Optional[T] {
 	if f.Done() {
 		return Of(f.Value)
 	}
